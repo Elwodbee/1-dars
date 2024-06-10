8b8124daa6461826858f0b9d6c7e5ed228bd7b80
Elwodbee <yojamshid87@gmail.com>
2024-06-10 14:21:20 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let day = prompt('Hafta kunini raqamini yozing') 
+
+switch (day) {
+    case '1': console.log(('Dushanba')); break
+    case '2': console.log(('Seshanba')); break
+    case '3': console.log(('Chorshanba')); break
+    case '4': console.log(('Payshanba')); break
+    case '5': console.log(('Juma')); break
+    case '6': console.log(('Shanba')); break
+    case '7': console.log(('Yakshanba')); break
+    default: console.log('bunday kun yoq');
+}
